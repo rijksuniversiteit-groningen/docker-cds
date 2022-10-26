92fc899d88b64a4fc9b9066e460eee14807530de
Venustiano <v.soancatl.aguilar@rug.nl>
2022-10-26 18:53:50 +0200

    GeneNetTools

---
+++ GeneNetTools/c_gene.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# Copyright (c) University of Groningen Data Science Team
+# Distributed under the terms of the Apache License 2.0
+
+library(GeneNetTools)
+
+# test if there are at least two arguments: if not, return an error
+if (length(args)==0) {
+  help(package=GeneNetTools)
+} else if (length(args)>=2) {
+  func <- args[1]
+  parameters <- args[2]
+
+  if (parameters == "help")
+     switch( func,
+          "c_pcor_shrunk" = ?c_pcor_shrunk,
+          # "c_violin" =    ?c_violin,
+          # "c_pcaproj" = ?c_pcaproj,
+	  cat(paste0("'",func,"' function is not available"))
+	  )
+  else {
+    # reading and validating the list of parameters
+    cat(paste("Opening  parameters file:",parameters))
+    lparams <- validate_json_file(parameters)
+
+    switch( func,
+          "c_pcor_shrunk" = {
+                          # validating the parameters against the JSON schema
+                          validate_parameters(parameters,pschema="pcor_shrunk_schema.json")
+                          c_pcor_shrunk(lparams)
+                        },
+          cat(paste0("'",func,"' function is not available"))
+          )
+	  }
+}
+
+
+# Rscript c_gene.R <function> <path/parameters.json>
+# Rscript c_gene.R <function> help
